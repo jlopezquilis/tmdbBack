c5af8cd776ad10c6776a238f2689d89997e8711e
lopezju1 <lopezju1@cvut.cz>
2024-01-20 19:08:30 +0100

    feat(app-config): Adding application configuration for correct execution.

---
+++ src/main/java/com/jlopezquilis/tmdbBack/configuration/AppConfig.java
@@ -0,0 +1,17 @@
+package com.jlopezquilis.tmdbBack.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.client.RestTemplate;
+
+
+@Configuration
+@PropertySource(value = {"classpath:application.properties", "classpath:application-secret.properties" })
+public class AppConfig
+{
+
+    @Bean
+    public RestTemplate restTemplate(){return new RestTemplate();}
+
+}
